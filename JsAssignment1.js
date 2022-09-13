2c40c56e1da8c462939e721adaad429cf1aa64ca
Dontavious Green <dontavious.o.green@gmail.com>
2022-09-13 17:54:00 -0500

    JsAssignment1prj

---
+++ JsAssignment1.js
@@ -0,0 +1,18 @@
+const inch = 1;
+const feet = inch*12;
+// calculation for 1 foot = 1 mile
+const yard = feet*3;
+const mile = feet*440;
+const trail = mile*10;
+const Jane = 3;
+const John = 8;
+let x = (John/Jane)*mile;
+
+    console.log(inch)
+console.log(feet)
+console.log(yard)
+console.log(mile)
+console.log(x)
+
+//document.getElementById("output01").innerHTML =
+    "Jane is (x) inches from John!";
