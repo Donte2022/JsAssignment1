bda2955b684b05dfbf585a51ad5e1f24f268ae44
Dontavious Green <dontavious.o.green@gmail.com>
2022-09-13 21:24:38 -0500

    JsAssignment1prj

--- JsAssignment1.js
+++ JsAssignment1.js
@@ -1,18 +1,24 @@
+
 const inch = 1;
-const feet = inch*12;
-// calculation for 1 foot = 1 mile
-const yard = feet*3;
-const mile = feet*440;
-const trail = mile*10;
-const Jane = 3;
-const John = 8;
-let x = (John/Jane)*mile;
+//Finds inches per Foot
+const InchPerFoot = inch*12;
+//Finds Foot per Yard
+const FootPerYard = InchPerFoot*3;
+//Finds Yard per Mile
+const YardPerMile = 1760;
+//Convert Yards per Mile into Inches per Mile
+const YardtoFeet = YardPerMile*3;
+//Jane's current distance on trail
+let Jane = 3;
+//John's current distance on trail
+let John = 8;
+//Finds distance from John to Jane
+let milestoJohn = John-Jane;
+//Breaks down distance from John to Jane into inches
+const x = milestoJohn*YardtoFeet;
 
-    console.log(inch)
-console.log(feet)
-console.log(yard)
-console.log(mile)
-console.log(x)
+console.log("Jane is " + x + " inches from John!")
 
-//document.getElementById("output01").innerHTML =
-    "Jane is (x) inches from John!";
+document.getElementById("output01").innerHTML =
+    "Jane is " + x + " inches from John!";
+
